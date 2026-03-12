5d10cd0a843b9882ee70e726af8b1e8adeb39692
utkxrshrxj <rajutkarsh.3004@gmail.com>
2026-03-12 09:48:52 +0530

    UC2: Basic Room Types & Static Availability

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,19 +1,59 @@
+abstract class Room {
+    String type;
+    int beds;
+    double price;
+
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Number of Beds: " + beds);
+        System.out.println("Price per Night: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 50.0);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 90.0);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 150.0);
+    }
+}
 
 public class BookMyStayApp {
-
     public static void main(String[] args) {
+        SingleRoom single = new SingleRoom();
+        DoubleRoom doubleR = new DoubleRoom();
+        SuiteRoom suite = new SuiteRoom();
 
-        // Application name and version
-        String appName = "Book My Stay - Hotel Booking System";
-        String version = "Version 1.0";
+        int singleAvailable = 5;
+        int doubleAvailable = 3;
+        int suiteAvailable = 2;
 
-        // Welcome message
-        System.out.println("=====================================");
-        System.out.println("        Welcome to " + appName);
-        System.out.println("              " + version);
-        System.out.println("=====================================");
-        System.out.println("Your trusted partner for easy hotel bookings!");
-        System.out.println("Application started successfully.");
-        System.out.println("=====================================");
+        System.out.println("Book My Stay - Hotel Booking System v2.1");
+        System.out.println("========================================\n");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailable + "\n");
+
+        doubleR.displayDetails();
+        System.out.println("Available: " + doubleAvailable + "\n");
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailable + "\n");
     }
 }
